afe4d0c8f7f869b52a2737bb33f331baf5f6cd65
Jeroen Overschie <jeroenoverschie@gmail.com>
2020-10-13 09:59:28 +0200

    Updates

---
+++ precache-manifest.b5aa88e0672fe1ce09477be3ebe952c6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4db477d8036326d179d8",
+    "url": "/bookshelf/static/css/main.038c493a.chunk.css"
+  },
+  {
+    "revision": "4db477d8036326d179d8",
+    "url": "/bookshelf/static/js/main.4db477d8.chunk.js"
+  },
+  {
+    "revision": "9f847fa1386fac93c830",
+    "url": "/bookshelf/static/js/1.9f847fa1.chunk.js"
+  },
+  {
+    "revision": "7924e80eec9c193a5b56",
+    "url": "/bookshelf/static/js/runtime~main.7924e80e.js"
+  },
+  {
+    "revision": "55d6b3813ad5a7cee6b283dbd171ec36",
+    "url": "/bookshelf/index.html"
+  }
+];
